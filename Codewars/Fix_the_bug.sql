7335618e42c62b8e599d3e7c4f4b45203ca2f242
Gauthami <45719872+gauthamikuravi@users.noreply.github.com>
2021-02-28 20:29:19 -0500

    Update and rename Fix_the_bug to Fix_the_bug.sql

--- Codewars/Fix_the_bug.sql
+++ Codewars/Fix_the_bug.sql
@@ -1,3 +1,4 @@
+/*
 Oh no! Timmys been moved into the database divison of his software company but as we know Timmy loves making mistakes. Help Timmy keep his job by fixing his query...
 
 Timmy works for a statistical analysis company and has been given a task of calculating the highest average salary for a given job, the sample is compiled of 100 applicants each with a job and a salary. Timmy must display each unique job, the total average salary, the total people and the total salary and order by highest average salary. Timmy has some bugs in his query, help Timmy fix his query so he can keep his job!
@@ -16,7 +17,7 @@ average_salary (float, 2 dp)
 total_people (int)
 total_salary (float, 2 dp)
 NOTE: Your solution should use pure SQL. Ruby is used within the test cases to do the actual testing.
-
+*/
 
 
 /* specifying output table columns but with casting to ensure decimals are properly formatted */ 
