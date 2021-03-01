f593e27e18cac5c118849681a373fbe6617eae01
Gauthami <45719872+gauthamikuravi@users.noreply.github.com>
2021-02-28 20:09:42 -0500

    Update and rename Recursion#1-Factorial to Recursion#1-Factorial.sql

--- Codewars/Recursion#1-Factorial.sql
+++ Codewars/Recursion#1-Factorial.sql
@@ -1,15 +1,15 @@
-#This is a kata series that you can only solve using recursion.
-##1 - Factorial
+--This is a kata series that you can only solve using recursion.
+-- ##1 - Factorial
 
-#In mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n. For example,
+-- #In mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n. For example,
 
-#5! = 5 * 4 * 3 * 2 * 1 = 120.
+-- #5! = 5 * 4 * 3 * 2 * 1 = 120.
 
-### The value of 0! is 1.
+-- ### The value of 0! is 1.
 
-#Your task
+-- #Your task
 
-### You have to create the function factorial that receives n and returns n!. You have to use recursion.
+--### You have to create the function factorial that receives n and returns n!. You have to use recursion.
 
 
 
